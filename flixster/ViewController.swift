3f0d4acb3d53b3a6e1be9326656bfba9465ebc35
Hanjing Zhu <hzhu7@nd.edu>
2020-09-24 16:42:19 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Hanjing Zhu on 9/24/20.
+//  Copyright © 2020 Hanjing Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
