181b203b4d68a9c70b02612949dc2394d816ca90
Hanjing Zhu <hzhu7@nd.edu>
2020-10-02 10:30:01 -0400

    Week 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Hanjing Zhu on 10/1/20.
+//  Copyright © 2020 Hanjing Zhu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
